7859111363155aea833e5c714ea713739f51ebad
Akash-nath29 <anath5440@gmail.com>
2023-09-23 23:33:43 +0530

    Design Update

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function showResult(){
+function showResult() {
     const searchBar = document.querySelector('#location');
     const selection = document.querySelector('#place');
+    const container = document.querySelector('.search')
     // console.log(selection.options[ selection.selectedIndex ].value);
-    searchBar.value = selection.options[ selection.selectedIndex ].value;
+    searchBar.value = selection.options[selection.selectedIndex].value;
+    if (selection.options[selection.selectedIndex].value == "Kalyani") {
+        container.innerHTML = '<!-- search result --><div class="searchResult" ><div class="leftSection col"><h1>Deepak Malhotra</h1><p>Contact: +91 6598326478<br>Address: Some Street, Moon<br>With 6 years of experience of recycling wastes</p></div><div class="middleSection col"><h1>Sell Waste</h1><a href="tel:1111222233"><button id="sellBtn">Contact To Sell</button></a></div><img src="img/factory1.jpeg" class="img-fluid col" alt="factory 1"></div><!-- search result --><div class="searchResult"><div class="leftSection col"><h1>Rajiv Deshmukh</h1><p>Contact: +91 1565798423<br>Address: Some Street, Jupitar<br>With 8 years of experience of recycling wastes</p></div><div class="middleSection col"> <h1>Sell Waste</h1><a href="tel:1111222233"><button id="sellBtn">Contact To Sell</button></a></div><img src="img/factory2.jpeg" class="img img-fluid col" alt="factory 1"></div>';
+    }
+    else if(selection.options[selection.selectedIndex].value == "Krishnanagar"){
+        container.innerHTML = ' <!-- search result --><div class="searchResult"><div class="leftSection col"><h1>Alok Tiwari</h1><p>Contact: +91 2356498715<br>Address: Some Street, Uranus<br>With 2 years of experience of recycling wastes</p></div><div class="middleSection col"><h1>Sell Waste</h1><a href="tel:1111222233"><button id="sellBtn">Contact To Sell</button></a></div><img src="img/factory3.jpg" class="img-fluid col" alt="factory 1"></div><!-- search result --><div class="searchResult"><div class="leftSection col"><h1>Sunil Kumar</h1><p>Contact: +91 9784563215<br>Address: Some Street, Pluto<br>With 5 years of experience of recycling wastes</p></div><div class="middleSection col"><h1>Sell Waste</h1><a href="tel:1111222233"><button id="sellBtn">Contact To Sell</button></a></div><img src="img/factory4.jpg" class="img-fluid col" alt="factory 1"></div>';
+    }
+
+    else if(selection.options[selection.selectedIndex].value == "Kolkata"){
+        container.innerHTML = ' <!-- search result --><div class="searchResult"><div class="leftSection col"><h1>Karan Rajan</h1><p>Contact: +91 5649871235<br>Address: Some Street, Sun<br>With 15 years of experience of recycling wastes</p></div><div class="middleSection col"><h1>Sell Waste</h1><a href="tel:1111222233"><button id="sellBtn">Contact To Sell</button></a></div><img src="img/factory5.jpeg" class="img-fluid col" alt="factory 1"></div><!-- search result --><div class="searchResult"><div class="leftSection col"><h1>Rakesh Sharma</h1><p>Contact: +91 3469782654<br>Address: Some Street, Saturn<br>With 4 years of experience of recycling wastes</p></div><div class="middleSection col"><h1>Sell Waste</h1><a href="tel:1111222233"><button id="sellBtn">Contact To Sell</button></a></div><img src="img/factory6.jpg" class="img-fluid col" alt="factory 1"></div>';
+    }
 }
