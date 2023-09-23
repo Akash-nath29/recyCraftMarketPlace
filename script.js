80114afa7b3ecd63659256a8214df3222663cfbd
Akash-nath29 <anath5440@gmail.com>
2023-09-23 19:57:27 +0530

    Design Update: Made mobile Friendly

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const showResult = () =>{
+function showResult(){
     const searchBar = document.querySelector('#location');
     const selection = document.querySelector('#place');
-    console.log(selection.options[ selection.selectedIndex ].value);
+    // console.log(selection.options[ selection.selectedIndex ].value);
     searchBar.value = selection.options[ selection.selectedIndex ].value;
 }
